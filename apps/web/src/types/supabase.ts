d1741cbd6a819d3515db9e570d09bfc9836afa2d
Sungmin Lee <sungminlee417@gmail.com>
2025-07-13 17:05:13 -0700

    fix: remove invalid type

--- apps/web/src/types/supabase.ts
+++ apps/web/src/types/supabase.ts
@@ -2,4 +2,3 @@ import type { Database } from "../../supabase/types/supabase"
 
 export type FoodEntry = Database['public']['Tables']['food_entries']['Row']
 export type Food = Database['public']['Tables']['foods']['Row']
-export type User = Database['public']['Tables']['users']['Row']
