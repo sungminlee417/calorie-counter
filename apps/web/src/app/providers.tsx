179777c547dedbc5f134bb4130865e8d973c54d3
Sungmin Lee <sungminlee417@gmail.com>
2025-07-14 23:46:12 -0700

    fix: be sure to wait for light/dark mode to load

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode, useMemo, useState } from "react";
+import React, { ReactNode, useEffect, useMemo, useState } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -9,19 +9,17 @@ import { ThemeModeProvider, useThemeMode } from "../context/ThemeModeContext";
 
 function InnerProviders({ children }: { children: ReactNode }) {
   const { mode } = useThemeMode();
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: { mode },
-      }),
-    [mode]
-  );
-
+  const [mounted, setMounted] = useState(false);
   const [queryClient] = useState(() => new QueryClient());
 
+  useEffect(() => setMounted(true), []);
+
+  const theme = useMemo(() => createTheme({ palette: { mode } }), [mode]);
+
+  if (!mounted) return null;
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} key={mode}>
       <CssBaseline />
       <QueryClientProvider client={queryClient}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
